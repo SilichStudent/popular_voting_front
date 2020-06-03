35cd25540411fd13ce54f99d1c6e6673cabc9e1f
s.silich <s.silich@softarex.com>
2020-06-03 12:26:04 +0300

    add view all

--- src/components/picket_map.component.jsx
+++ src/components/picket_map.component.jsx
@@ -14,7 +14,7 @@ export class PicketMap extends Component {
             {this.props.candidates
               .filter(
                 (candidate) =>
-                this.props.selectedCandidates.indexOf(candidate.id) > -1
+                this.props.selectedCandidates === 0 || this.props.selectedCandidates.indexOf(candidate.id) > -1
               )
               .map((candidate) => {
                 return candidate.coordinates
